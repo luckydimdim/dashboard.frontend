71c213a90196d12c735fe39e07690ab82dbf1867
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-01-25 10:38:35 +0300

    finished chart

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -22,18 +22,18 @@ class DashboardComponent implements OnInit {
 
   @override
   void ngOnInit() {
-    var script = new ScriptElement()
+    /*var script = new ScriptElement()
       ..async = true
       ..type = 'text/javascript'
       ..src = 'vendor/chart.js/dist/Chart.min.js';
     document.body.append(script);
-    script.onLoad.listen(initChart);
+    script.onLoad.listen(initChart);*/
 
-    //_resourcesLoaderService.loadScript('vendor/chart.js/dist/', 'Chart.min.js', false);
-    //_resourcesLoaderService.loadScript('packages/dashboard/src/', 'dashboard-init.js', false);
+    _resourcesLoaderService.loadScript('vendor/chart.js/dist/', 'Chart.min.js', false);
+    _resourcesLoaderService.loadScript('packages/dashboard/src/', 'dashboard-init.js', false);
   }
 
   void initChart(Event e) {
-    _resourcesLoaderService.loadScript('packages/dashboard/src/', 'dashboard-init.js', false);
+    /*_resourcesLoaderService.loadScript('packages/dashboard/src/', 'dashboard-init.js', false);*/
   }
 }
