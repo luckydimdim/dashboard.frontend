9af141f8a010ac666efdbae7fbed42aad5ed91f0
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-01-27 17:09:06 +0300

    gauge

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
 import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 
 import 'package:resources_loader/resources_loader.dart';
 import 'package:chartjs/chartjs.dart';
+import 'package:gauge/gauge.dart';
 
 import 'dart:html';
 import 'dart:math' as math;
@@ -27,6 +29,36 @@ class DashboardComponent implements OnInit {
   void ngOnInit() {
     _resourcesLoaderService.loadScript(
         'vendor/chart.js/dist/', 'Chart.min.js', false, onData: initMainChart);
+
+    //initGauge1();
+  }
+
+  initGauge1() {
+    var grayDark =      '#2a2c36';
+    var brandInfo =     '#63c2de';
+    var grayLighter =   '#d1d4d7';
+
+    ResourcesLoaderService resourcesLoader = new ResourcesLoaderService();
+
+    var options = new GaugeOptions()
+      ..angle = 0.15
+      ..lineWidth = 0.44
+      ..pointer = new PointerOptions(length: 0.8,strokeWidth:0.035,color: grayDark )
+      ..limitMax = false
+      ..colorStart = brandInfo
+      ..colorStop = brandInfo
+      ..strokeColor = grayLighter
+      ..generateGradient = true;
+
+    var gauge = new Gauge(resourcesLoader, querySelector('#gauge') as CanvasElement, options);
+
+    gauge.Init().then((_){
+      gauge.maxValue = 3000; // set max gauge value
+      gauge.animationSpeed = 32; // set animation speed (32 is default value)
+      gauge.set(2000); // set actual value
+    });
+
+
   }
 
   void initMainChart() {
