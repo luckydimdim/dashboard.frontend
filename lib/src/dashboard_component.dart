d181887cc4ee449df88ae07e0096848da907fe1c
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-01-31 13:56:24 +0300

    set width of tables

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -9,8 +9,7 @@ import 'package:gauge/gauge.dart';
 import 'dart:html';
 import 'dart:math' as math;
 
-@Component(selector: 'dashboard',
-    templateUrl: 'dashboard_component.html')
+@Component(selector: 'dashboard', templateUrl: 'dashboard_component.html')
 class DashboardComponent implements OnInit {
   static const String route_name = 'Dashboard';
   static const String route_path = 'dashboard';
@@ -28,100 +27,117 @@ class DashboardComponent implements OnInit {
   @override
   void ngOnInit() {
     _resourcesLoaderService.loadScript(
-        'vendor/chart.js/dist/', 'Chart.min.js', false, onData: initMainChart);
+        'vendor/chart.js/dist/', 'Chart.min.js', false,
+        onData: initMainChart);
 
     initGauge1();
-    initGauge2();
   }
 
   Future initGauge1() async {
-    var grayDark =      '#2a2c36';
-    var brandInfo =     '#63c2de';
-    var grayLighter =   '#d1d4d7';
+    var grayDark = '#2a2c36';
+    var brandInfo = '#63c2de';
+    var grayLighter = '#d1d4d7';
 
     var options = new GaugeOptions()
       ..angle = 0.35
       ..lineWidth = 0.1
-      ..pointer = new PointerOptions(length: 0.9,strokeWidth:0.035,color: grayDark )
+      ..pointer =
+          new PointerOptions(length: 0.9, strokeWidth: 0.035, color: grayDark)
       ..limitMax = false
       ..colorStart = brandInfo
       ..colorStop = brandInfo
       ..strokeColor = grayLighter
       ..generateGradient = true;
 
-    var gauge = new Donut(this._resourcesLoaderService, querySelector('#gauge1') as CanvasElement, options);
+    var gauge = new Donut(this._resourcesLoaderService,
+        querySelector('#gauge1') as CanvasElement, options);
 
     var success = await gauge.Init();
 
-    if (success){
+    if (success) {
       gauge.maxValue = 100; // set max gauge value
       gauge.animationSpeed = 32; // set animation speed (32 is default value)
       gauge.set(77); // set actual value
     }
-
-  }
-
-  Future initGauge2() async {
-    var grayDark =      '#2a2c36';
-    var brandInfo =     '#63c2de';
-    var grayLighter =   '#d1d4d7';
-
-    var options = new GaugeOptions()
-      ..angle = 0.35
-      ..lineWidth = 0.1
-      ..pointer = new PointerOptions(length: 0.9,strokeWidth:0.035,color: grayDark )
-      ..limitMax = false
-      ..colorStart = brandInfo
-      ..colorStop = brandInfo
-      ..strokeColor = grayLighter
-      ..generateGradient = true;
-
-    var gauge = new Donut(this._resourcesLoaderService, querySelector('#gauge2') as CanvasElement, options);
-
-    var success = await gauge.Init();
-
-    if (success){
-      gauge.maxValue = 100; // set max gauge value
-      gauge.animationSpeed = 32; // set animation speed (32 is default value)
-      gauge.set(28); // set actual value
-    }
-
   }
 
   void initMainChart() {
-
     var brandInfo = '#63c2de';
-    var brandSuccess =  '#4dbd74';
+    var brandSuccess = '#4dbd74';
 
-    var xAxes = new List<ChartXAxe> ();
+    var xAxes = new List<ChartXAxe>();
     xAxes.add(
         new ChartXAxe(gridLines: new GridLineOptions(drawOnChartArea: false)));
 
-    var yAxes = new List<ChartYAxe> ();
-    yAxes.add(new ChartYAxe(ticks: new LinearTickOptions(
-        beginAtZero: true, maxTicksLimit: 5, stepSize: 1000000, max: 6700000)));
+    var yAxes = new List<ChartYAxe>();
+    yAxes.add(new ChartYAxe(
+        ticks: new LinearTickOptions(
+            beginAtZero: true,
+            maxTicksLimit: 5,
+            stepSize: 1000000,
+            max: 6700000)));
 
     var chartScales = new ChartScales(xAxes: xAxes, yAxes: yAxes);
 
     var legend = new ChartLegendOptions(display: false);
 
-    var chartElementsOptions = new ChartElementsOptions(point: new ChartPointOptions(radius: 0, hitRadius: 10,hoverRadius: 4, hoverBorderWidth: 3 ));
+    var chartElementsOptions = new ChartElementsOptions(
+        point: new ChartPointOptions(
+            radius: 0, hitRadius: 10, hoverRadius: 4, hoverBorderWidth: 3));
 
-    var chartOptions = new ChartOptions(responsive: true,
+    var chartOptions = new ChartOptions(
+        responsive: true,
         maintainAspectRatio: false,
         legend: legend,
         scales: chartScales,
-        elements: chartElementsOptions
-    );
+        elements: chartElementsOptions);
 
-    var data1 = <int>[700000, 1000000, 1100000, 1200000, 1700000, 2700000, 3700000, 4200000, 4700000, 5200000, 5700000, 6200000];
-    var data2 = <int>[700000, 1000000, 1100000, 1200000, 1700000, 2650000, 3650000, 4100000, 4200000, 4700000, 5200000, 5700000];
+    var data1 = <int>[
+      700000,
+      1000000,
+      1100000,
+      1200000,
+      1700000,
+      2700000,
+      3700000,
+      4200000,
+      4700000,
+      5200000,
+      5700000,
+      6200000
+    ];
+    var data2 = <int>[
+      700000,
+      1000000,
+      1100000,
+      1200000,
+      1700000,
+      2650000,
+      3650000,
+      4100000,
+      4200000,
+      4700000,
+      5200000,
+      5700000
+    ];
 
-    var data = new LinearChartData(labels: <String>['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-        datasets: <ChartDataSets>[
+    var data = new LinearChartData(labels: <String>[
+      '01',
+      '02',
+      '03',
+      '04',
+      '05',
+      '06',
+      '07',
+      '08',
+      '09',
+      '10',
+      '11',
+      '12'
+    ], datasets: <ChartDataSets>[
       new ChartDataSets(
           label: 'My First dataset',
-          backgroundColor: 'rgba(99,194,222,0.1)',//convertHex($.brandInfo,10),
+          backgroundColor: 'rgba(99,194,222,0.1)', //convertHex($.brandInfo,10),
           borderColor: brandInfo,
           pointHoverBackgroundColor: '#fff',
           borderWidth: 2,
@@ -135,9 +151,8 @@ class DashboardComponent implements OnInit {
           data: data2)
     ]);
 
-
-    var config = new ChartConfiguration(
-        type: 'line', data: data, options: chartOptions);
+    var config =
+        new ChartConfiguration(type: 'line', data: data, options: chartOptions);
 
     new Chart(querySelector('#main-chart') as CanvasElement, config);
   }
