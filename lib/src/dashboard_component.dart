5a71d0d4b58ce318b2c7ebc1d8cbf9955504e206
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-02-20 16:20:44 +0300

    HtmlElement

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -26,7 +26,7 @@ class DashboardComponent implements OnInit {
 
   // import 'dart:html';
   void breadcrumbInit(){
-    var  breadcrumbContent = querySelector('#breadcrumbContent') as DivElement;
+    var  breadcrumbContent = querySelector('#breadcrumbContent') as HtmlElement;
 
     if (breadcrumbContent == null)
       return;
