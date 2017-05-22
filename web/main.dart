2cd0677244bf8db5ec3856799db0d8d2c01cab63
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-05-22 14:47:12 +0300

    ng 3.1.0 maintenance

--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,17 @@
 import 'dart:core';
 
+import 'package:alert/src/alert_service.dart';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
-import 'package:angular2/src/core/reflection/reflection.dart';
 import 'package:angular2/router.dart';
 import 'package:angular2/src/platform/browser/location/hash_location_strategy.dart';
 import 'package:angular2/platform/common.dart';
 
+import 'package:aside/aside_service.dart';
+import 'package:auth/auth_service.dart';
+import 'package:config/config_service.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 import 'package:resources_loader/resources_loader.dart';
 import 'package:master_layout/master_layout_component.dart';
 
@@ -23,19 +28,20 @@ bool get isDebug =>
 class AppComponent {}
 
 main() async {
-  if (isDebug) {
-    reflector.trackUsage();
-  }
-
   ComponentRef ref = await bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
-    const Provider(ResourcesLoaderService, useClass: ResourcesLoaderService)
+    const Provider(ResourcesLoaderService, useClass: ResourcesLoaderService),
+    const Provider(AuthenticationService),
+    const Provider(AuthorizationService),
+    const Provider(ConfigService),
+    const Provider(AsideService),
+    const Provider(AlertService),
+    provide(Client, useFactory: () => new BrowserClient(), deps: [])
   ]);
 
   if (isDebug) {
     print('Application in DebugMode');
     enableDebugTools(ref);
-    print('Unused keys: ${reflector.listUnusedKeys()}');
   }
 }
